0b051203ddb633028af2513bdd59ce2795d8480b
Lourival Menezes <lourival.m@aluno.ifsp.edu.br>
2021-09-05 11:51:59 -0300

    Add project files.

---
+++ TCCApp/TCCApp/EsqSenhaPage.xaml.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace TCCApp
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class EsqSenhaPage : ContentPage
+    {
+        public EsqSenhaPage()
+        {
+            InitializeComponent();
+            NavigationPage.SetHasNavigationBar(this, false);
+        }
+    }
+}
---
+++ TCCApp/TCCApp/MainPage.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace TCCApp
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+            NavigationPage.SetHasNavigationBar(this, false);
+           
+        }
+
+        private async void EsqSenhaButton_Clicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new EsqSenhaPage());
+        }
+
+        private async void EntrarButton_Clicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new HistoryPage());
+        }
+
+        private async void CadastroButton_Clicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new CadastroPage());
+        }
+    }
+}
