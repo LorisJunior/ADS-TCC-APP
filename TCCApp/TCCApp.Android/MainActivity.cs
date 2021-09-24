0fb37dddc7e52f8de3c918a126842ab8819c3ea8
Lourival Menezes <lourival.m@aluno.ifsp.edu.br>
2021-09-23 22:29:00 -0300

    Adicionado UI da página AddItemPage.

--- TCCApp/TCCApp.Android/MainActivity.cs
+++ TCCApp/TCCApp.Android/MainActivity.cs
@@ -13,7 +13,6 @@ namespace TCCApp.Droid
         protected override void OnCreate(Bundle savedInstanceState)
         {
             base.OnCreate(savedInstanceState);
-
             Xamarin.Essentials.Platform.Init(this, savedInstanceState);
             global::Xamarin.Forms.Forms.Init(this, savedInstanceState);
             Xamarin.FormsMaps.Init(this, savedInstanceState);
