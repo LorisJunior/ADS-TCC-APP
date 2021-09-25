25fa50cf84f54ef6dc83f6e68d60c2c680561469
Lourival Menezes <lourival.m@aluno.ifsp.edu.br>
2021-09-25 04:40:59 -0300

    Adicionado image pick e novos icones para o PerfilPage.

--- TCCApp/TCCApp/PerfilPage.xaml.cs
+++ TCCApp/TCCApp/PerfilPage.xaml.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using Xamarin.Essentials;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
@@ -16,6 +16,22 @@ namespace TCCApp
         {
             InitializeComponent();
             NavigationPage.SetHasNavigationBar(this, false);
+        }
+
+        private async void ImageButton_Clicked(object sender, EventArgs e)
+        {
+            try
+            {
+                var media = await MediaPicker.PickPhotoAsync(new MediaPickerOptions
+                {
+                    Title = "Pega uma foto"
+                });
+                var stream = await media.OpenReadAsync();
+                image.Source = ImageSource.FromStream(() => stream);
+            }
+            catch (NullReferenceException)
+            {
+            }
         }
     }
 }
