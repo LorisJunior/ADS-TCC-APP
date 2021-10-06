4e5a6771f21fb8c94915e7dc9d10f29585639d82
Lourival <em-lourival@outlook.com>
2021-10-06 11:27:36 -0300

    Removido o Xamarin.Forms.Maps

--- TCCApp/TCCApp.Android/MainActivity.cs
+++ TCCApp/TCCApp.Android/MainActivity.cs
@@ -22,7 +22,6 @@ namespace TCCApp.Droid
             GoogleClientManager.Initialize(this);
             Xamarin.Essentials.Platform.Init(this, savedInstanceState);
             global::Xamarin.Forms.Forms.Init(this, savedInstanceState);
-            Xamarin.FormsMaps.Init(this, savedInstanceState);
             LoadApplication(new App(new OAuth2Service()));
         }
         public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
